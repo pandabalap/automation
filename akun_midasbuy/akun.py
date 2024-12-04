feddc80d3f929f7bc8e863e44358ef683424ca00
Abbaynihbos <abbyaryansa5@gmail.com>
2024-12-04 23:00:18 +0700

    code baru

--- akun_midasbuy/akun.py
+++ akun_midasbuy/akun.py
@@ -5,12 +5,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
+import random
+from datetime import datetime, timedelta
 import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
+
+def generate_random_date():
+    start_date = datetime(1990, 1, 1)
+    end_date = datetime(1999, 12, 31)
+
+    days_range = (end_date - start_date).days
+    random_days = random.randint(0, days_range)
+    random_date = start_date + timedelta(days=random_days)
+    return random_date.strftime("%d %m %Y")
 
 driver.implicitly_wait(10)
 
@@ -19,11 +30,17 @@ def close_pop_up():
         driver.get('https://www.midasbuy.com/midasbuy/id/buy/pubgm')
 
         try:
-            WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="root"]/div/div[13]')))
-            print("pop up muncul!")
+            # WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="root"]/div/div[13]')))
+            # print("pop up 1 muncul!")
 
-            driver.find_element(By.CLASS_NAME, "PatFacePopWrapper_close-btn__erWAb").click()
-            print("pop up close!")
+            # driver.find_element(By.CLASS_NAME, "PatFacePopWrapper_close-btn__erWAb").click()
+            # print("pop up close!")
+
+            WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="app"]/html/body/div[4]')))
+            print("pop up 2 muncul !")
+            
+            driver.find_element(By.CLASS_NAME, "cumulativeRecharge-close_1TD7P").click()
+            print("pop up 2 close!")
 
         except TimeoutException:
             print("gagal close pop up!")
@@ -48,6 +65,12 @@ def log_in():
     input_pass.send_keys(input(f"Input Password : "))
     time.sleep(0.5)
 
+    random_date = generate_random_date()
+    print("Tanggal : ", random_date)
+    date_input = driver.find_element(By.XPATH, '//*[@id="login-sdk-app"]/div[1]/div/div[3]/div[1]/div[4]/div[2]/div[1]/div/input')
+    date_input.send_keys(random_date)
+    print("tanggal Oke !")
+    time.sleep(0.5)
 
 close_pop_up()
 log_in()
