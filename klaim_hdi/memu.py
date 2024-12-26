af7964aca2ce5e742c5470574353b117b38ace67
macminicoy <sambelmatah888@gmail.com>
2024-12-27 02:54:30 +0700

    update_27

--- klaim_hdi/memu.py
+++ klaim_hdi/memu.py
@@ -2,22 +2,8 @@ from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-
-# Konfigurasi Desired Capabilities untuk emulator Memu
-desired_caps = {
-    "platformName": "Android",       # Platform yang digunakan
-    "deviceName": "Android Device",   # Nama perangkat emulator
-    "appPackage": "com.neptune.dominogl",  # Ganti dengan package aplikasi yang dituju
-    "appActivity": "com.pokercity.lobby.lobby",   # Ganti dengan activity utama aplikasi
-    "udid": "127.0.0.1:26625",
-    "automationName": "UiAutomator2",
-    "noReset": True                   # Tidak mengatur ulang data aplikasi
-}
-
-# Membuat koneksi ke Appium Server
-driver = webdriver.Remote("http://localhost:4723", desired_caps)
-
-driver.implicitly_wait(10)
+import configparser
+import threading
 
 # Koordinat untuk mengklik (contoh klik inbox pada game)
 klik_inbox = {"x" : 970 ,"y": 825}
@@ -27,12 +13,33 @@ klik_receive = {"x" : 1005 ,"y": 735}
 klik_confirm = {"x" : 800 ,"y": 705}
 klik_exit = {"x" : 1520 ,"y": 195}
 
+def run_automation(section):
+    config = configparser.ConfigParser()
+    config.read("config.cfg")
+    
+    devicename = config.get(section, "devicename")
+    udid = config.get(section, "udid")
+    remote_url = config.get(section, "remote_url")
+    
+    
+    # Konfigurasi Desired Capabilities untuk emulator Memu
+    desired_caps = {
+        "platformName": "Android",       # Platform yang digunakan
+        "deviceName": devicename,   # Nama perangkat emulator
+        "appPackage": "com.neptune.dominogl",  # Ganti dengan package aplikasi yang dituju
+        "appActivity": "com.pokercity.lobby.lobby",   # Ganti dengan activity utama aplikasi
+        "udid": udid,
+        "automationName": "UiAutomator2",
+        "noReset": True                   # Tidak mengatur ulang data aplikasi
+    }
 
+    # Membuat koneksi ke Appium Server
+    driver = webdriver.Remote(remote_url, desired_caps)
+    driver.implicitly_wait(10)
+    return driver
 
-actions = ActionChains(driver)
-
-def proses_awal():
-    print("Proses awal berjalan...")
+def proses_awal(driver):
+    actions = ActionChains(driver)
 
     actions.w3c_actions.pointer_action.move_to_location(klik_inbox["x"],klik_inbox["y"])
     actions.w3c_actions.pointer_action.click()
@@ -66,7 +73,8 @@ def proses_awal():
     time.sleep(1)
     
     
-def proses_dua():
+def proses_dua(driver):
+    actions = ActionChains(driver)
     print("Proses kedua tanpa Refres berjalan...")
         
     actions.w3c_actions.pointer_action.move_to_location(klik_inbox["x"],klik_inbox["y"])
@@ -95,9 +103,29 @@ def proses_dua():
     actions.perform()
     time.sleep(1)
 
+
+def worker(section, jumlah_klaim):
+    driver = run_automation(section)
+    try: 
+        print(f"[{section}] Proses awal Berjalan. . .")
+        proses_awal(driver)
+        for i in range(jumlah_klaim):
+            print(f"[{section}] Proses Klaim ke-{i + 1}. . .")
+            proses_dua(driver)
+            time.sleep(0.5)
+        print(f"[{section}] Klaim Selesai.")
+    except Exception as e:
+        print(f"[{section}] Terjadi error: {e}")
+    finally:
+        driver.quit()
+
 def main_proses():
-    try:    
-        while True:
+    config = configparser.ConfigParser()
+    config.read("config.cfg")
+    sections = config.sections()
+    
+    jumlah_klaim = 0
+    while True:
             try :
                 jumlah_klaim = int(input("Masukkan jumlah : "))
                 if jumlah_klaim > 0:
@@ -107,23 +135,15 @@ def main_proses():
                     
             except ValueError :
                 print("Salah blok, masukkan angka ")
-            
-        proses_awal()
-        while True:    
-            for i in range(jumlah_klaim):
-                print(f"Proses klaim ke-{i + 1}...")
-                try:
-                    proses_dua()
-                    time.sleep(0.5)
-                except Exception as e:
-                    print(f"Error order ke-{i + 1}: {e}")
-                    
-            print("Klaim Sukses All !")
-            break
-         
-    finally:
-        if driver:
-            driver.quit()
+    
+    threads = []
+    for section in sections:
+        thread = threading.Thread(target=worker, args=(section, jumlah_klaim))
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
         
 if __name__ == "__main__":
     main_proses()
